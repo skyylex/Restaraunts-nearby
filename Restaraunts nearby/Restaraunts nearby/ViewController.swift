fe21ceccc7063a85d6c42697f3a12f1685948790
Yury Lapitsky <yury.lapitsky@gmail.com>
2021-02-04 09:06:32 +0100

    Blank xcode project

---
+++ Restaraunts nearby/Restaraunts nearby/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaraunts nearby
+//
+//  Created by Yury Lapitsky on 04/02/2021.
+//  Copyright © 2021 Yury Lapitsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Restaraunts nearby/Restaraunts nearbyTests/Restaraunts_nearbyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Restaraunts_nearbyTests.swift
+//  Restaraunts nearbyTests
+//
+//  Created by Yury Lapitsky on 04/02/2021.
+//  Copyright © 2021 Yury Lapitsky. All rights reserved.
+//
+
+import XCTest
+@testable import Restaraunts_nearby
+
+class Restaraunts_nearbyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
