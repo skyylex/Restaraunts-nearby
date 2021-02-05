71fe0bfab4c0c5ec5c89625bdb039d2ec3e4888e
Yury Lapitsky <yury.lapitsky@gmail.com>
2021-02-05 23:08:38 +0100

    Creation of RootCoordinator

---
+++ Restaraunts/Restaurants/Coordinator/RootCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  RootCoordinator.swift
+//  Restaurants
+//
+//  Created by Yury Lapitsky on 05/02/2021.
+//  Copyright © 2021 Yury Lapitsky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Initiates application UX flow
+final class RootCoordinator {
+    
+    /// Starts UX Flow with Map screen
+    /// - Parameter window: UIWindow to render Map
+    func start(with window: UIWindow) {
+        let mapViewModel = MapViewModel()
+        let rootViewController = MapViewController()
+        rootViewController.viewModelInput = mapViewModel
+        rootViewController.viewModelOutput = mapViewModel
+        
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+    }
+    
+}
--- Restaraunts/Restaurants/SceneDelegate.swift
+++ Restaraunts/Restaurants/SceneDelegate.swift
@@ -11,27 +11,22 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var rootCoordinator: RootCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        window = UIWindow(windowScene: windowScene)
+        let window = UIWindow(windowScene: windowScene)
         
-        let mapViewModel = MapViewModel()
-        let rootViewController = MapViewController()
-        rootViewController.viewModelInput = mapViewModel
-        rootViewController.viewModelOutput = mapViewModel
+        rootCoordinator = RootCoordinator()
+        rootCoordinator?.start(with: window)
         
-        window?.rootViewController = rootViewController
-        window?.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+        window = nil
+        rootCoordinator = nil
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
